8b01f7280b1545ff29e502d025a6a86b02773507
GuilhermeLopesCardoso <guilhermelopescardoso5@gmail.com>
2024-03-21 21:58:14 -0300

    Alterações

--- POO2/src/exercicio_arquivos/Jogo.java
+++ POO2/src/exercicio_arquivos/Jogo.java
@@ -3,6 +3,7 @@ package exercicio_arquivos;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JOptionPane;
 
@@ -13,14 +14,16 @@ public class Jogo {
 
 
         try {
+            int nm = 0;
             FileReader fr = new FileReader("Perguntas.txt");
             BufferedReader br = new BufferedReader(fr);
             String linha = "";
             while ((linha = br.readLine())!=null) {
+                nm ++;
                 String v [] = linha.split(",");
                 String pergunta = v[0];
                 String resposta = v[1];
-                perguntas.add(new Pergunta(pergunta, resposta));
+                perguntas.add(new Pergunta(pergunta, resposta, nm));
             }
             br.close();
         } catch(Exception e) {
@@ -40,13 +43,24 @@ public class Jogo {
             if(op==1) {
                 perguntas(perguntas);
             }
+            if(op==2) {
+                numeros();
+            }
         } while(op!=3);
 
     }
 
+    public static void numeros() {
+        Random gerador = new Random();
+
+        for(int i = 1;i < 10; i++) {
+            System.out.println(gerador.nextInt(20));
+        }
+    }
+
     public static void perguntas(ArrayList<Pergunta> perguntas) {
         for(Pergunta p : perguntas) {
-            System.out.println(p.getPergunta());
+            System.out.println(p.getPergunta()+ " - " + p.getNumero());
         }
     }
 }
--- POO2/src/exercicio_arquivos/Pergunta.java
+++ POO2/src/exercicio_arquivos/Pergunta.java
@@ -3,11 +3,13 @@ package exercicio_arquivos;
 public class Pergunta {
     String pergunta;
     String resposta;
+    int numero;
     
     
-    public Pergunta(String pergunta, String resposta) {
+    public Pergunta(String pergunta, String resposta, int numero) {
     	this.pergunta = pergunta;
     	this.resposta = resposta;
+        this.numero = numero;
     }
     
     public String getPergunta() {
@@ -22,6 +24,16 @@ public class Pergunta {
     public void setResposta(String resposta) {
         this.resposta = resposta;
     }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    
 
     
 }
