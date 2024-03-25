59ece6627fb40917d711113942231458c5c0f850
GuilhermeLopesCardoso <guilhermelopescardoso5@gmail.com>
2024-03-25 20:01:55 -0300

    Correção Exercício Arquivo

--- POO2/src/atividade_arquivos_correcao/Pergunta.java
+++ POO2/src/atividade_arquivos_correcao/Pergunta.java
@@ -1,35 +1,37 @@
 package atividade_arquivos_correcao;
 
 public class Pergunta {
-    private String pergunta;
-    private boolean resposta;
-
-
-    public Pergunta criaPergunta (String linha) {
-        String[] dados = linha.split(",");
-        setPergunta(dados[0]);
-       setResposta(dados[0], dados[1].equals("Verdadeiro"));
-    }
-
-    public Pergunta(String pergunta, boolean resposta) {
-        super();
-        this.pergunta = pergunta;
-        this.resposta = resposta;
-    }
-
     
-    public String getPergunta() {
-        return pergunta;
-    }
-    public void setPergunta(String pergunta) {
-        this.pergunta = pergunta;
-    }
-    public boolean isResposta() {
-        return resposta;
-    }
-    public void setResposta(boolean resposta) {
-        this.resposta = resposta;
-    }
+    private String titulo;
+	private boolean respostaCorreta;
+	
+	public Pergunta (String linha) {
+		String[] dados = linha.split(",");
+		setTitulo(dados[0]);
+		setRespostaCorreta(dados[1].equals("Verdadeiro"));
+	}
+	
+	public Pergunta() {
+		
+	}
+	
+	public Pergunta(String titulo, boolean respostaCorreta) {
+		this.titulo = titulo;
+		this.respostaCorreta = respostaCorreta;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public boolean isRespostaCorreta() {
+		return respostaCorreta;
+	}
+	public void setRespostaCorreta(boolean respostaCorreta) {
+		this.respostaCorreta = respostaCorreta;
+	}
+	
+	
 
-    
 }
